6f2f37b7137b1c2fad5b8e58fce9b47665a82a79
Jill Marcum <jill.marcum@gmail.com>
2020-04-27 09:46:03 -0700

    adding Checkout form

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+
+import Button from '../../../components/UI/Button/Button';
+import classes from './ContactData.module.css';
+
+class ContactData extends Component {
+  state = {
+    name: '',
+    email: '',
+    address: {
+      street: '',
+      postalCode: '',
+    }
+  }
+
+  orderHandler = (event) => {
+    event.preventDefault();
+    console.log(this.props.ingredients);
+  }
+
+  render() {
+    return (
+      <div className={classes.ContactData}>
+        <h4>Enter your Contact Data.</h4>
+        <form>
+          <input className={classes.Input} type='text' name='name' placeholder='Name' />
+          <input className={classes.Input} otype='email' name='email' placeholder='Email' />
+          <input className={classes.Input} type='text' name='street' placeholder='Street' />
+          <input className={classes.Input} type='text' name='postal' placeholder='PostalCode' />
+          <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
+        </form>
+      </div>
+    );
+  }
+
+};
+
+export default ContactData;
