8d993c15a6cfe0d912996e452ddfadbc8fb1c0dc
Jill Marcum <jill.marcum@gmail.com>
2020-05-23 14:47:07 -0700

    redirect user to checkout after auth

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -42,6 +42,13 @@ class Auth extends Component {
     },
     isSignUp: true,
   }
+
+  componentDidMount () {
+    if(!this.buildingBurrito && this.props.authRedirectPath !== '/') {
+      this.props.onSetAuthRedirectPath();
+    };
+  };
+
   checkValidity(value, rules) {
       let isValid = true;
       if (!rules) {
@@ -133,7 +140,7 @@ class Auth extends Component {
 
     let authRedirect = null;
     if (this.props.isAuthenticated) {
-      authRedirect = <Redirect to='/' />
+      authRedirect = <Redirect to={this.props.authRedirectPath} />
     };
 
     return(
@@ -157,12 +164,15 @@ const mapStateToProps = state => {
     loading: state.auth.loading,
     error: state.auth.error,
     isAuthenticated: state.auth.token !== null,
+    buildingBurrito: state.burritoBuilder.building,
+    authRedirectPath: state.auth.authRedirectPath,
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
+    onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/')),
   };
 };
 
