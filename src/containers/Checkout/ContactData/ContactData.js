0d4bceb1b800cbbe156d29db732da78cf624b6db
Jill Marcum <jill.marcum@gmail.com>
2020-05-23 16:26:26 -0700

    fix semicolon

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -105,6 +105,7 @@ class ContactData extends Component {
      ingredients: this.props.ings,
      price: this.props.price,
      orderData: formData,
+     userId: this.props.userId,
     }
     this.props.onOrderBurrito(order, this.props.token);
   }
@@ -203,6 +204,7 @@ const mapStateToProps = state => {
     price: state.burritoBuilder.totalPrice,
     loading: state.order.loading,
     token: state.auth.token,
+    userId: state.auth.userId,
   };
 };
 
