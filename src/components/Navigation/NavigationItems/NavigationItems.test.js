07a0a9d10f9cda02099fe592986e99b456b1648a
Jill Marcum <jill.marcum@gmail.com>
2020-05-24 18:51:47 -0700

    improve navitem test

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -4,7 +4,7 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import NavigationItems from './NavigationItems';
-import NavigationItem from './NavigationItems/NavigationItem'
+import NavigationItem from './NavigationItem/NavigationItem'
 
 configure({ adapter: new Adapter()});
 
