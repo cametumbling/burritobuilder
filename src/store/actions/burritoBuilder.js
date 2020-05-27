82e44ba3537002820c3855df418666ba58901f23
Jill Marcum <jill.marcum@gmail.com>
2020-05-26 18:24:37 -0700

    add burritoBuilder side effects to sagas

--- src/store/actions/burritoBuilder.js
+++ src/store/actions/burritoBuilder.js
@@ -1,5 +1,4 @@
 import * as actionTypes from './actionTypes';
-import axios from '../../axios-orders';
 
 export const addIngredient = (name) => {
   return {
@@ -29,13 +28,16 @@ export const fetchIngredientsFailed = () => {
 };
 
 export const initIngredients = () => {
-  return dispatch => {
-    axios.get('https://burritobuilder-15294.firebaseio.com/ingredients.json')
-      .then(response => {
-        dispatch(setIngredients(response.data));
-      })
-      .catch(error => {
-        dispatch(fetchIngredientsFailed());
-      });
+  return {
+    types: actionTypes.INIT_INGREDIENTS,
   };
+  // return dispatch => {
+  //   axios.get('https://burritobuilder-15294.firebaseio.com/ingredients.json')
+  //     .then(response => {
+  //       dispatch(setIngredients(response.data));
+  //     })
+  //     .catch(error => {
+  //       dispatch(fetchIngredientsFailed());
+  //     });
+  // };
 };
